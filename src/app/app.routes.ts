5816b429e4e3b96eef55f0ebfde6ddf9b3d0d07a
Gonza-Herrera <maxi_1902@hotmail.com>
2025-11-30 04:03:41 -0300

    feature: se agrega spinner y not found component para proteger rutas en la app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './features/not-found/not-found.component';
 
 export const routes: Routes = [
 { path: '', redirectTo: 'clients', pathMatch: 'full' },
 { path: 'auth', loadChildren: () => import('./features/auth/auth.routes').then(m => m.routes) },
 { path: 'clients', loadChildren: () => import('./features/clients/clients.routes').then(m => m.routes) },
-//{ path: '**', component: NotFoundComponent },
+{ path: '**', loadComponent: () => import('./features/not-found/not-found.component').then(m => m.NotFoundComponent) }
 ];
